b706ad14cda43c1b9ca2436ac003e56e810938a1
Uros Trstenjak <uros.trstenjak@gmail.com>
2019-07-09 20:08:11 +0200

    add navigation between pages

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+const About = () => (
+  <div>
+    <p>About</p>
+  </div>
+);
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
+import Link from 'next/link';
+
 const Index = () => (
   <div>
+    <Link href='/about'>
+      <a title='About page'>About</a>
+    </Link>
     <p>Hello Next.js</p>
   </div>
 );
